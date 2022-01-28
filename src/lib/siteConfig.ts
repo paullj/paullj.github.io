205f16cccb55c313cf7a8a19d2b5469c1de80573
Paul Lavender-Jones <paul@lavender-jones.com>
2022-01-28 17:11:18 +0000

    first commit

---
+++ src/lib/siteConfig.ts
@@ -0,0 +1,7 @@
+export const SITE_URL = 'https://paullj.github.io';
+export const GH_USER_REPO = 'paullj/paullj.github.io'; // used for pulling github issues and offering comments
+export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
+export const SITE_TITLE = 'Paul Lavender-Jones';
+export const SITE_DESCRIPTION = '';
+export const DEFAULT_OG_IMAGE =
+	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';

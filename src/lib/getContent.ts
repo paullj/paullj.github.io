2d9c67dc6a2b3fb5bdfc6c13fb11949811f03fd0
Paul Lavender-Jones <paul@lavender-jones.com>
2022-01-31 22:29:50 +0000

    🐛 Changes to auth token for accessing github api

--- src/lib/getContent.ts
+++ src/lib/getContent.ts
@@ -1,128 +1,134 @@
-import { GITHUB_USER, GITHUB_REPO } from './siteConfig';
-import { dev } from '$app/env';
+import { GITHUB_USER, GITHUB_REPO } from "./siteConfig";
+import { dev } from "$app/env";
 
-import { request as requestWithoutAuth } from '@octokit/request';
+import { request as requestWithoutAuth } from "@octokit/request";
 
-import slugify from 'slugify';
-import { compile } from 'mdsvex';
-import oembed from 'remark-oembed';
-import sanitize from 'rehype-sanitize';
+import slugify from "slugify";
+import { compile } from "mdsvex";
+import oembed from "remark-oembed";
 
-const publishedLabels = ['published'];
+const publishedLabels = ["published"];
 let posts = [];
 
-const request = requestWithoutAuth.defaults({
-	headers: {
-		authorization: 'token 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'
-	}
-});
+const githubToken = process.env.GITHUB_TOKEN;
+
+const request = process.env.GITHUB_TOKEN
+  ? requestWithoutAuth.defaults({
+      headers: {
+        authorization: `token ${githubToken}`,
+      },
+    })
+  : requestWithoutAuth;
 
 const getComments = async (issueNumber) => {
-	const results = await request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-		owner: GITHUB_USER,
-		repo: GITHUB_REPO,
-		issue_number: issueNumber
-	});
+  const results = await request(
+    "GET /repos/{owner}/{repo}/issues/{issue_number}/comments",
+    {
+      owner: GITHUB_USER,
+      repo: GITHUB_REPO,
+      issue_number: issueNumber,
+    }
+  );
 
-	if (results.status === 200) {
-		return results.data.map((comment) => {
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const { url, total_count, ...reactions } = comment.reactions;
+  if (results.status === 200) {
+    return results.data.map((comment) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { url, total_count, ...reactions } = comment.reactions;
 
-			return {
-				user: {
-					name: comment.user.name,
-					login: comment.user.login,
-					author: comment.user.login === GITHUB_USER,
-					avatar: comment.user.avatar_url,
-					url: comment.user.url
-				},
-				createdAt: new Date(comment.created_at),
-				updatedAt: new Date(comment.updated_at),
-				content: comment.body,
-				totalReactions: total_count,
-				reactions,
-				url: comment.html_url
-			};
-		});
-	}
-	throw new Error(results.status);
+      return {
+        user: {
+          name: comment.user.name,
+          login: comment.user.login,
+          author: comment.user.login === GITHUB_USER,
+          avatar: comment.user.avatar_url,
+          url: comment.user.url,
+        },
+        createdAt: new Date(comment.created_at),
+        updatedAt: new Date(comment.updated_at),
+        content: comment.body,
+        totalReactions: total_count,
+        reactions,
+        url: comment.html_url,
+      };
+    });
+  }
+  throw new Error(results.status);
 };
 
 const getPosts = async () => {
-	const results = await request(`GET /repos/{owner}/{repo}/issues`, {
-		owner: GITHUB_USER,
-		repo: GITHUB_REPO,
-		state: 'closed',
-		creator: GITHUB_USER,
-		labels: publishedLabels.join(','),
-		per_page: 100
-	});
+  const results = await request(`GET /repos/{owner}/{repo}/issues`, {
+    owner: GITHUB_USER,
+    repo: GITHUB_REPO,
+    state: "closed",
+    creator: GITHUB_USER,
+    labels: publishedLabels.join(","),
+    per_page: 100,
+  });
 
-	if (results.status === 200) {
-		const issues = results.data
-			.map((issue) => {
-				// eslint-disable-next-line @typescript-eslint/no-unused-vars
-				const { url, total_count, ...reactions } = issue.reactions;
+  if (results.status === 200) {
+    const issues = results.data
+      .map((issue) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { url, total_count, ...reactions } = issue.reactions;
 
-				return {
-					title: issue.title,
-					slug: slugify(issue.title).toLowerCase(),
-					url: issue.html_url,
-					issueNumber: issue.number,
-					publishedAt: new Date(issue.closed_at),
-					createdAt: new Date(issue.created_at),
-					updatedAt: new Date(issue.updated_at),
-					totalReactions: total_count,
-					totalComments: issue.comments,
-					reactions,
-					content: issue.body
-				};
-			})
-			.sort((a, b) => +b.publishedAt - +a.publishedAt);
+        return {
+          title: issue.title,
+          slug: slugify(issue.title).toLowerCase(),
+          url: issue.html_url,
+          issueNumber: issue.number,
+          publishedAt: new Date(issue.closed_at),
+          createdAt: new Date(issue.created_at),
+          updatedAt: new Date(issue.updated_at),
+          totalReactions: total_count,
+          totalComments: issue.comments,
+          reactions,
+          content: issue.body,
+        };
+      })
+      .sort((a, b) => +b.publishedAt - +a.publishedAt);
 
-		posts = issues;
-	}
+    posts = issues;
+  }
 
-	return posts;
+  return posts;
 };
 
 const getPost = async (slug: string) => {
-	if (dev || !posts || posts.length === 0) {
-		posts = await getPosts();
-	}
+  if (dev || !posts || posts.length === 0) {
+    posts = await getPosts();
+  }
 
-	if (!posts.length) throw new Error('No posts!');
+  if (!posts.length) throw new Error("No posts!");
 
-	const post = posts.find((post) => post.slug === slug);
-	if (post) {
-		const content = (
-			await compile(post.content, {
-				remarkPlugins: [[oembed, { syncWidget: true }]]
-			})
-		).code;
+  const post = posts.find((post) => post.slug === slug);
+  if (post) {
+    const content = (
+      await compile(post.content, {
+        remarkPlugins: [[oembed, { syncWidget: true }]],
+      })
+    ).code;
 
-		const comments = await getComments(post.issueNumber);
-		const commentsPromises = comments.map((comment) =>
-			compile(comment.content, {
-				remarkPlugins: [
-					// sanitize FIXME: this is taking out all html
-				]
-			})
-		);
-		const commentsContent = await Promise.all(commentsPromises);
+    const comments = await getComments(post.issueNumber);
+    const commentsPromises = comments.map((comment) =>
+      compile(comment.content, {
+        remarkPlugins: [
+          // sanitize FIXME: this is taking out all html
+        ],
+      })
+    );
+    const commentsContent = await Promise.all(commentsPromises);
 
-		return {
-			...post,
-			content,
-			comments: comments.map((comment, i) => ({
-				...comment,
-				content: commentsContent[i].code
-			}))
-		};
-	}
+    return {
+      ...post,
+      content,
+      comments: comments.map((comment, i) => ({
+        ...comment,
+        content: commentsContent[i].code,
+      })),
+    };
+  }
 
-	throw new Error(`Can't find post, '${slug}'`);
+  throw new Error(`Can't find post, '${slug}'`);
 };
 
 export { getPosts, getPost };
--- src/lib/siteConfig.ts
+++ src/lib/siteConfig.ts
@@ -1,8 +1,8 @@
-export const SITE_URL = 'https://paullj.github.io';
-export const GITHUB_USER = 'paullj';
-export const GITHUB_REPO = 'paullj.github.io';
+export const SITE_URL = "https://paullj.github.io";
+export const GITHUB_USER = "paullj";
+export const GITHUB_REPO = "paullj.github.io";
 export const GITHUB_REPO_URL = `https://github.com/${GITHUB_USER}/${GITHUB_REPO}`;
-export const SITE_TITLE = 'Paul Lavender-Jones';
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = "Paul Lavender-Jones";
+export const SITE_DESCRIPTION = "";
 export const DEFAULT_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+  "https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png";

42da32718cf5c491efcc870ae9bbec0eadd9fc7c
Paul Lavender-Jones <paul@lavender-jones.com>
2022-02-04 14:38:06 +0000

    Updates

---
+++ _app/pages/posts/index.svelte-a486a175.js
@@ -0,0 +1,1 @@
+import{S as t,i as e,s as n}from"../../chunks/vendor-0edfcedb.js";const r=async()=>({status:303,redirect:"/"});class l extends t{constructor(s){super();e(this,s,null,null,n,{})}}export{l as default,r as load};
--- _app/pages/posts/index.svelte-d6686664.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as e,s as n}from"../../chunks/vendor-18abff92.js";const r=async()=>({status:303,redirect:"/"});class l extends t{constructor(s){super();e(this,s,null,null,n,{})}}export{l as default,r as load};
